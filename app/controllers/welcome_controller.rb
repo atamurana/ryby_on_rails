9965cdd080cc4d1c12de9c80a9ef96ec9b728146
atamurana <atamurana@list.ru>
2016-11-02 03:53:10 +0300

    add render parsial, add links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,23 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+	def index
+	end
+	def the_super_remote
+	end
+	def compatibility
+	end
+	def gallery
+	end
+	def reviews_and_press
+	end
+	def help
+	end
+	def about_us
+	end
+	def careers
+	end
+	def order_status
+	end
+	def legal
+	end
+
 end
